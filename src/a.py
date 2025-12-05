f64c4c114fe6fa7bfa325c0aad723f50aace347a
W1nd0w55 <iliader@outlook.com>
2025-12-05 11:25:30 +0000

    added tests

--- src/a.py
+++ src/a.py
@@ -1,4 +1,4 @@
 def div(a, b):
     if b == 0:
-        raise ValueError("Division by zero is not allowed.")
+        raise ZeroDivisionError("Division by zero is not allowed.")
     return a / b
---
+++ testing/test_a.py
@@ -0,0 +1,23 @@
+import pytest
+from src.a import div
+
+@pytest.mark.parametrize('a, b, excpected', [
+    (1, 2, 0.5),
+    (2, 3, 0.6666666666666666),
+    (3, 4, 0.75),
+    (4, 5, 0.8),
+    (5, 6, 0.83333333333333333),
+    (6, 7, 0.8571428571428571),
+    (7, 8, 0.875),
+    (8, 9, 0.8888888888888888),
+])
+def test_division(a, b, excpected):
+    assert div(a, b) == excpected
+
+def test_zero_division():
+    with pytest.raises(ZeroDivisionError):
+        div(1, 0)
+
+def test_infinity_division():
+    assert div(float('inf'), 1) == float('inf')
+    assert div(1, float('inf')) == 0.0
